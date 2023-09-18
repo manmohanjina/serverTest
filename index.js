00820489391757d084c43124f3a4af2437f81da6
manmohanjina <manish63singh@gmail.com>
2023-09-18 14:16:39 +0530

    serVertTest

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { ApiGetFun } = require("./apiCalling");
+app.use(cors());
+app.use(express.json());
 
+app.get("/", async (req, res) => {
+  const api = req.body.url;
+  console.log(api);
+  try {
+    let data = await ApiGetFun(api);
+    console.log(data, "data");
+    return res.json({ res: data });
+  } catch (error) {
+    console.log(error, "internal sever error");
+  }
+});
 
-const express=require('express')
-const app=express()
-const cors=require('cors')
-const { ApiGetFun } = require('./apiCalling')
-app.use(cors())
-app.use(express.json())
-
-
-
-
-app.get('/',async(req,res)=>{
-    const api=req.body.url   
-    console.log(api)
-    try {
-     
-let data=await ApiGetFun(api)
- console.log(data,"data")
- return res.json({res:data})
-
-    } catch (error) {
-        console.log(error,"internal sever error")
-    }
-})
-
-
-
-app.listen(8090,()=>{
-    console.log('running')
-})
+app.listen( process.env.PORT||8080, () => {
+  console.log("running");
+});
